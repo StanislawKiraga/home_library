07fd7e82924af317b261a9d6bc8344a4760f6180
StanislawKiraga <kiragasta@gmail.com>
2023-05-08 14:30:12 +0200

    m2m book-author

--- app/models.py
+++ app/models.py
@@ -2,26 +2,33 @@ from app import db
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, index=True)
+    title = db.Column(db.String(100), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-    borrowed = db.relationship('Borrowed', backref='book', lazy='select')
+    read = db.Column(db.Boolean, default=False)
+    borrowed = db.Column(db.Boolean, default=False)
 
     def __str__(self):
         return f'<Book {self.id}'
     
+books_authors = db.Table('books',
+                         db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
+                         db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True)
+ )
+
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    books = db.relationship('Book', backref='author', lazy='select')
+    name = db.Column(db.String(100), nullable=False)
+    books = db.relationship('Book', backref=db.backref('author'), lazy=True)
 
     def __str__(self):
-        return f'<Author {self.id}>'
+        return f'{self.name}'
     
 class Borrowed(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    available = db.Column(db.String)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
 
     def __str__(self):
         return f'<Available: {self.available}>'
+
+    
